58ab35b2bb0fa68012104c8ab4f393032f480a58
jiwon <pjw99032986@gmail.com>
2024-09-15 16:29:05 +0900

    feat : 필요한 라이브러리 임포트

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("androidx.navigation.safeargs") version "2.8.0" apply false
 }
