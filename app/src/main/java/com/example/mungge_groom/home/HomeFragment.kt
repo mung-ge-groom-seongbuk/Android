101b6c99e2d89022c82f9f50869e0dcb8da803b2
jiwon <pjw99032986@gmail.com>
2024-09-15 16:29:28 +0900

    feat : 홈 프레그먼트 임포트

---
+++ app/src/main/java/com/example/mungge_groom/home/HomeFragment.kt
@@ -0,0 +1,17 @@
+package com.example.mungge_groom.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mungge_groom.R
+import com.example.mungge_groom.databinding.FragmentHomeBinding
+import com.example.umc_stepper.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun setLayout() {
+
+    }
+
+}
