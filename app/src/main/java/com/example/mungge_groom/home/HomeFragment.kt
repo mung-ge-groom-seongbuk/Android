0e1e43281386c4e187b96779d669a8d9d0dcbfd1
jiwon <pjw99032986@gmail.com>
2024-09-27 20:03:27 +0900

    feat : 홈

--- app/src/main/java/com/example/mungge_groom/home/HomeFragment.kt
+++ app/src/main/java/com/example/mungge_groom/home/HomeFragment.kt
@@ -1,12 +1,50 @@
 package com.example.mungge_groom.home
 
+import androidx.fragment.app.Fragment
 import com.example.mungge_groom.R
-import com.example.mungge_groom.databinding.FragmentHomeBinding
 import com.example.mungge_groom.base.BaseFragment
+import com.example.mungge_groom.base.FragmentAdapter
+import com.example.mungge_groom.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    private lateinit var pageList: List<Fragment>
     override fun setLayout() {
-
+        setPageFragment()
     }
+
+    private fun setPageFragment() {
+        pageList = listOf(Toggle1Fragment(), Toggle2Fragment())
+
+        val fragmentStateAdapter = FragmentAdapter(requireActivity())
+        fragmentStateAdapter.fragments = pageList
+        binding.pager.adapter = fragmentStateAdapter
+
+        val tabTitle = listOf("match", "map")
+
+        TabLayoutMediator(binding.tab, binding.pager) { tab, position ->
+            tab.text = tabTitle[position]
+        }.attach()
+
+        binding.tab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(p0: TabLayout.Tab?) {
+                p0?.let {
+                    binding.pager.currentItem = it.position
+                }
+            }
+            override fun onTabUnselected(p0: TabLayout.Tab?) {
+                p0?.let {
+
+                }
+            }
+            override fun onTabReselected(p0: TabLayout.Tab?) {
+                p0?.let {
+
+                }
+            }
+        })
+    }
+
 
 }
