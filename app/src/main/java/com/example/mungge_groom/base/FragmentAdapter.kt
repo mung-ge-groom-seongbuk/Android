9b8a7f52bb55c648374cfa4a1274d07522e00ab4
jiwon <pjw99032986@gmail.com>
2024-09-27 20:03:35 +0900

    feat : 프레그먼트 어댑터

---
+++ app/src/main/java/com/example/mungge_groom/base/FragmentAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.mungge_groom.base
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class FragmentAdapter(fragmentList : FragmentActivity) : FragmentStateAdapter(fragmentList) {
+    var fragments = listOf<Fragment>()
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+
+
+}
