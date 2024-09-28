9d9b45bf2db6cf0409924e7421319517bac0a43d
jiwon <pjw99032986@gmail.com>
2024-09-28 15:41:42 +0900

    refactor : ui 이동

--- app/src/main/java/com/example/mungge_groom/ui/home/Toggle1Fragment.kt
+++ app/src/main/java/com/example/mungge_groom/ui/home/Toggle1Fragment.kt
@@ -1,13 +1,8 @@
 package com.example.mungge_groom.ui.home
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.mungge_groom.R
-import com.example.mungge_groom.ui.base.BaseFragment
 import com.example.mungge_groom.databinding.FragmentToggle1Binding
+import com.example.mungge_groom.ui.base.BaseFragment
 
 
 class Toggle1Fragment : BaseFragment<FragmentToggle1Binding>(R.layout.fragment_toggle1) {
