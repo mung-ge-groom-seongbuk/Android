488598d76016a066eeab6848e045178999e224f2
jiwon <pjw99032986@gmail.com>
2024-09-30 16:00:27 +0900

    feat : 채팅 리스트 데이터 클래스 추가

---
+++ app/src/main/java/com/example/mungge_groom/data/response/ChatListData.kt
@@ -0,0 +1,9 @@
+package com.example.mungge_groom.data.response
+
+data class ChatListData(
+    val profile : String,
+    val nickName : String,
+    val description : String,
+    val arriveTime : String,
+    val messageCount : String
+)
