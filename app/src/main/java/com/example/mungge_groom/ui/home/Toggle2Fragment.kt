136ffb5544008a6702d4e773d52de030f9cbf7b4
jiwon <pjw99032986@gmail.com>
2024-10-04 16:50:30 +0900

    feat : 임시 플로팅 버튼 기능 코딩

--- app/src/main/java/com/example/mungge_groom/ui/home/Toggle2Fragment.kt
+++ app/src/main/java/com/example/mungge_groom/ui/home/Toggle2Fragment.kt
@@ -1,6 +1,7 @@
 package com.example.mungge_groom.ui.home
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
@@ -8,9 +9,12 @@ import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.fragment.findNavController
 import com.example.mungge_groom.R
 import com.example.mungge_groom.ui.base.BaseFragment
 import com.example.mungge_groom.databinding.FragmentToggle2Binding
+import com.example.mungge_groom.ui.listener.onClickBottomSheetDialogListener
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -21,7 +25,7 @@ import com.google.android.gms.maps.MapView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class Toggle2Fragment : BaseFragment<FragmentToggle2Binding>(R.layout.fragment_toggle2),
+class Toggle2Fragment : BaseFragment<FragmentToggle2Binding>(R.layout.fragment_toggle2),onClickBottomSheetDialogListener,
     OnMapReadyCallback {
 
     private lateinit var mapView: MapView
@@ -51,39 +55,51 @@ class Toggle2Fragment : BaseFragment<FragmentToggle2Binding>(R.layout.fragment_t
         binding.floatingStart.setOnClickListener {
             when (binding.floatingStart.tag) {
                 "start" -> {
-                    // 상태를 "pause"으로 변경하고 이미지 변경
-                    binding.floatingStart.tag = "pause"
-                    binding.floatingStart.foreground = ContextCompat.getDrawable(requireContext(), R.drawable.ic_button_pause)
+                    with(binding) {
+                        // 상태를 "pause"으로 변경하고 이미지 변경
+                        floatingStart.tag = "pause"
+                        floatingStart.foreground =
+                            ContextCompat.getDrawable(requireContext(), R.drawable.ic_button_pause)
+                    }
                 }
                 "pause" -> {
-                    binding.floatingStart.tag = "stop"
-                    // 상태를 "stop"로 변경하고 이미지 변경
-                    binding.floatingPlay.visibility = View.VISIBLE
-                    binding.floatingSquare.visibility = View.VISIBLE
-                    binding.floatingStart.visibility = View.GONE
+                    with(binding) {
+                        floatingStart.tag = "stop"
+                        // 상태를 "stop"로 변경하고 이미지 변경
+                        floatingPlay.visibility = View.VISIBLE
+                        floatingSquare.visibility = View.VISIBLE
+                        floatingStart.visibility = View.GONE
+                    }
                 }
-
             }
         }
 
         binding.floatingSquare.setOnClickListener{
-            binding.floatingStart.tag = "start"
-            binding.floatingStart.foreground = ContextCompat.getDrawable(requireContext(), R.drawable.ic_button_start)
-            binding.floatingStart.visibility = View.VISIBLE
-            binding.floatingPlay.visibility = View.GONE
-            binding.floatingSquare.visibility = View.GONE
+            with(binding) {
+                floatingStart.tag = "start"
+                floatingStart.foreground =
+                    ContextCompat.getDrawable(requireContext(), R.drawable.ic_button_start)
+                floatingStart.visibility = View.VISIBLE
+                floatingPlay.visibility = View.GONE
+                floatingSquare.visibility = View.GONE
+            }
         }
 
         binding.floatingPlay.setOnClickListener{
-            binding.floatingStart.tag = "pause"
-            binding.floatingStart.foreground = ContextCompat.getDrawable(requireContext(), R.drawable.ic_button_pause)
-            binding.floatingStart.visibility = View.VISIBLE
-            binding.floatingPlay.visibility = View.GONE
-            binding.floatingSquare.visibility = View.GONE
+            with(binding) {
+                floatingStart.tag = "pause"
+                floatingStart.foreground =
+                    ContextCompat.getDrawable(requireContext(), R.drawable.ic_button_pause)
+                floatingStart.visibility = View.VISIBLE
+                floatingPlay.visibility = View.GONE
+                floatingSquare.visibility = View.GONE
+            }
+        }
 
+        binding.ex.setOnClickListener{
+            showBottomSheetDialog()
         }
     }
-
 
     override fun onMapReady(map: GoogleMap) {
         googleMap = map
@@ -111,6 +127,11 @@ class Toggle2Fragment : BaseFragment<FragmentToggle2Binding>(R.layout.fragment_t
             // 권한 요청
             requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
+    }
+
+    private fun showBottomSheetDialog(){
+        val modal = HomeBottomSheetDialog(this)
+        modal.show(requireActivity().supportFragmentManager, "HomeBottomSheet")
     }
 
     // MapView의 생명주기 메서드 관리
@@ -147,5 +168,11 @@ class Toggle2Fragment : BaseFragment<FragmentToggle2Binding>(R.layout.fragment_t
     override fun onLowMemory() {
         super.onLowMemory()
         mapView.onLowMemory()
+    }
+
+    override fun onClick() {
+        // 바텀 네비게이션의 선택 상태 변경
+        val mainActivity = activity as MainActivity
+        mainActivity.setBottomNavMoveItem(R.id.chatListFragment,R.id.action_chatListFragment_to_chatRoomFragment)
     }
 }
