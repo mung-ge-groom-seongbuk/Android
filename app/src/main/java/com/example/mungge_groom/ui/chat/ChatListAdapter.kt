d95bf8d1e5831c39b617822cdd07a9173f4b5ae2
jiwon <pjw99032986@gmail.com>
2024-10-04 17:29:00 +0900

    feat : 채팅방 리스트 어댑터 추가

--- app/src/main/java/com/example/mungge_groom/ui/chat/ChatListAdapter.kt
+++ app/src/main/java/com/example/mungge_groom/ui/chat/ChatListAdapter.kt
@@ -8,8 +8,9 @@ import com.example.mungge_groom.databinding.ItemToggle1Binding
 import com.example.mungge_groom.extention.GlobalApplication
 import com.example.mungge_groom.ui.base.BaseAdapter
 import com.example.mungge_groom.ui.base.BaseDiffCallback
+import com.example.mungge_groom.ui.listener.onChatRoomClickedListener
 
-class ChatListAdapter : BaseAdapter<ChatListData, ItemChatListBinding> (
+class ChatListAdapter(private val onChatRoomClickedListener: onChatRoomClickedListener) : BaseAdapter<ChatListData, ItemChatListBinding> (
     BaseDiffCallback(
         itemsTheSame = { oldItem, newItem -> oldItem == newItem },
         contentsTheSame = { oldItem, newItem -> oldItem == newItem }
@@ -18,8 +19,26 @@ class ChatListAdapter : BaseAdapter<ChatListData, ItemChatListBinding> (
     override val layoutId: Int
         get() = R.layout.item_chat_list
 
+    private var originalList: List<ChatListData> = listOf() // 원본 리스트를 저장
+
+    // 데이터를 설정하는 메서드
+    fun setChatListData(dataList: List<ChatListData>) {
+        originalList = dataList
+        submitList(dataList)
+    }
+
     override fun bind(binding: ItemChatListBinding, item: ChatListData) {
         binding.chatListItem = item
+        binding.root.setOnClickListener{
+            onChatRoomClickedListener.onClick(item.nickName)
+        }
         GlobalApplication.loadProfileImage(binding.itemChatListIv,item.profile)
+    }
+
+    fun findChatData(query: String) {
+        val filteredList = originalList.filter {
+            it.nickName.contains(query, ignoreCase = true) || it.description.contains(query, ignoreCase = true)
+        }
+        submitList(filteredList)
     }
 }
