067beb4a56a0f296eb0fc1d1b05efa058c8fecbd
jiwon <pjw99032986@gmail.com>
2024-10-06 16:34:55 +0900

    refactor : 마커에 필요한 데이터 정리

---
+++ app/src/main/java/com/example/mungge_groom/data/model/MarkerData.kt
@@ -0,0 +1,7 @@
+package com.example.mungge_groom.data.model
+
+data class MarkerData(
+    val latitude: Double,
+    val longitude: Double,
+    val crewData: RunningCrewData?
+)
---
+++ app/src/main/java/com/example/mungge_groom/data/model/RunningCrewData.kt
@@ -0,0 +1,10 @@
+package com.example.mungge_groom.data.model
+
+data class RunningCrewData(
+    val name: String?,
+    val description: String?,
+    val count: String?,
+    val km: String?,
+    val pace: String?,
+    val profile: String?
+)
