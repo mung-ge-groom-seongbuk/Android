adb5a64cd2095beaa5997c29f8281b5ec5114ac4
jiwon <pjw99032986@gmail.com>
2024-10-10 02:03:49 +0900

    feat : API 연동 전 레포지토리 모듈 준비

--- app/src/main/java/com/example/mungge_groom/data/di/RepositoryModule.kt
+++ app/src/main/java/com/example/mungge_groom/data/di/RepositoryModule.kt
@@ -1,8 +1,14 @@
 package com.example.mungge_groom.data.di
 
 import com.example.mungge_groom.data.repository.AccountRepository
+import com.example.mungge_groom.data.repository.ChatRepository
+import com.example.mungge_groom.data.repository.HomeRepository
 import com.example.mungge_groom.data.source.account.AccountApiDataSource
 import com.example.mungge_groom.data.source.account.AccountApiRepositoryImpl
+import com.example.mungge_groom.data.source.chat.ChatDataSource
+import com.example.mungge_groom.data.source.chat.ChatRepositoryImpl
+import com.example.mungge_groom.data.source.home.HomeDataSource
+import com.example.mungge_groom.data.source.home.HomeRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,9 +18,20 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object RepositoryModule {
+
     @Singleton
     @Provides
     fun provideMainApiRepository(accountApiDataSource: AccountApiDataSource): AccountRepository =
         AccountApiRepositoryImpl(accountApiDataSource)
+
+    @Singleton
+    @Provides
+    fun provideHomeApiRepository(homeDataSource: HomeDataSource): HomeRepository =
+        HomeRepositoryImpl(homeDataSource)
+
+    @Singleton
+    @Provides
+    fun provideChatApiRepository(chatDataSource: ChatDataSource): ChatRepository =
+        ChatRepositoryImpl(chatDataSource)
 
 }
