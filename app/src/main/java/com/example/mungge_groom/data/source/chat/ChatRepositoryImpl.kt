db932b724a24582bb2ec370566158473801a8018
jiwon <pjw99032986@gmail.com>
2024-10-10 02:02:25 +0900

    feat : Chat API 연동 준비

--- app/src/main/java/com/example/mungge_groom/data/source/chat/ChatRepositoryImpl.kt
+++ app/src/main/java/com/example/mungge_groom/data/source/chat/ChatRepositoryImpl.kt
@@ -2,10 +2,21 @@ package com.example.mungge_groom.data.source.chat
 
 import com.example.mungge_groom.data.repository.AccountRepository
 import com.example.mungge_groom.data.repository.ChatRepository
+import com.example.mungge_groom.data.request.SendChatDTO
 import com.example.mungge_groom.data.source.account.AccountApiDataSource
+import com.example.mungge_groom.ui.base.BaseResponse
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ChatRepositoryImpl @Inject constructor(
     private val chatDataSource: ChatDataSource
 ) : ChatRepository {
+    override suspend fun postSendChat(sendChatDTO: SendChatDTO): Flow<BaseResponse<String>> =
+        chatDataSource.postSendChat(sendChatDTO)
+
+    override suspend fun getChatMessage(
+        senderId: String,
+        receiverId: String
+    ): Flow<BaseResponse<String>> = chatDataSource.getChatMessage(senderId,receiverId)
+
 }
