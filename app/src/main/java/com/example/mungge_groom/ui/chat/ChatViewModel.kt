1385b1714263a27eb0f0bd1faf35752a150d85bb
jiwon <pjw99032986@gmail.com>
2024-10-11 00:32:34 +0900

    feat : 채팅 뷰모델

--- app/src/main/java/com/example/mungge_groom/ui/chat/ChatViewModel.kt
+++ app/src/main/java/com/example/mungge_groom/ui/chat/ChatViewModel.kt
@@ -5,23 +5,27 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.mungge_groom.data.repository.ChatRepository
 import com.example.mungge_groom.data.request.SendChatDTO
+import com.example.mungge_groom.data.response.ChatRoomData
+import com.example.mungge_groom.data.response.ChatRoomResponse
 import com.example.mungge_groom.ui.base.BaseResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-
+@HiltViewModel
 class ChatViewModel @Inject constructor(
     private val chatRepository: ChatRepository
 ) : ViewModel(){
 
-    private val _chatSend = MutableStateFlow(BaseResponse<String>())
-    val chatSend : StateFlow<BaseResponse<String>> = _chatSend
+    private val _chatSend = MutableStateFlow(ChatRoomResponse())
+    val chatSend : StateFlow<ChatRoomResponse> = _chatSend
 
-    private val _chatMessage = MutableStateFlow(BaseResponse<String>())
-    val chatMessage : StateFlow<BaseResponse<String>> = _chatMessage
+    private val _chatMessage = MutableStateFlow(ChatRoomData())
+    val chatMessage : StateFlow<ChatRoomData> = _chatMessage
 
-    private fun postSendChat(sendChatDTO: SendChatDTO){
+    fun postSendChat(sendChatDTO: SendChatDTO){
         viewModelScope.launch {
             try {
                 chatRepository.postSendChat(sendChatDTO).collect{
@@ -34,7 +38,7 @@ class ChatViewModel @Inject constructor(
         }
     }
 
-    private fun getChatMessage(senderId: String,receiverId: String ){
+    fun getChatMessage(senderId: String,receiverId: String ){
         viewModelScope.launch {
             try {
                 chatRepository.getChatMessage(senderId,receiverId).collect{
