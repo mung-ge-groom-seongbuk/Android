7113af5cc6e60cdaac9d954e083e14c1a44ebf89
jiwon <pjw99032986@gmail.com>
2024-10-11 00:31:47 +0900

    feat : 채팅 서버 응답 데이터 클래스

--- app/src/main/java/com/example/mungge_groom/data/repository/ChatRepository.kt
+++ app/src/main/java/com/example/mungge_groom/data/repository/ChatRepository.kt
@@ -3,6 +3,8 @@ package com.example.mungge_groom.data.repository
 import com.example.mungge_groom.data.request.LogInDTO
 import com.example.mungge_groom.data.request.SendChatDTO
 import com.example.mungge_groom.data.request.SignUpDTO
+import com.example.mungge_groom.data.response.ChatRoomData
+import com.example.mungge_groom.data.response.ChatRoomResponse
 import com.example.mungge_groom.ui.base.BaseResponse
 import kotlinx.coroutines.flow.Flow
 import okhttp3.MultipartBody
@@ -11,7 +13,7 @@ import retrofit2.http.Part
 
 interface ChatRepository {
 
-    suspend fun postSendChat(sendChatDTO: SendChatDTO) : Flow<BaseResponse<String>>
-    suspend fun getChatMessage(senderId : String, receiverId : String) : Flow<BaseResponse<String>>
+    suspend fun postSendChat(sendChatDTO: SendChatDTO) : Flow<ChatRoomResponse>
+    suspend fun getChatMessage(senderId : String, receiverId : String) : Flow<ChatRoomData>
 
 }
--- app/src/main/java/com/example/mungge_groom/data/source/chat/ChatRepositoryImpl.kt
+++ app/src/main/java/com/example/mungge_groom/data/source/chat/ChatRepositoryImpl.kt
@@ -3,6 +3,8 @@ package com.example.mungge_groom.data.source.chat
 import com.example.mungge_groom.data.repository.AccountRepository
 import com.example.mungge_groom.data.repository.ChatRepository
 import com.example.mungge_groom.data.request.SendChatDTO
+import com.example.mungge_groom.data.response.ChatRoomData
+import com.example.mungge_groom.data.response.ChatRoomResponse
 import com.example.mungge_groom.data.source.account.AccountApiDataSource
 import com.example.mungge_groom.ui.base.BaseResponse
 import kotlinx.coroutines.flow.Flow
@@ -11,12 +13,11 @@ import javax.inject.Inject
 class ChatRepositoryImpl @Inject constructor(
     private val chatDataSource: ChatDataSource
 ) : ChatRepository {
-    override suspend fun postSendChat(sendChatDTO: SendChatDTO): Flow<BaseResponse<String>> =
-        chatDataSource.postSendChat(sendChatDTO)
+    override suspend fun postSendChat(sendChatDTO: SendChatDTO): Flow<ChatRoomResponse> = chatDataSource.postSendChat(sendChatDTO)
 
     override suspend fun getChatMessage(
         senderId: String,
         receiverId: String
-    ): Flow<BaseResponse<String>> = chatDataSource.getChatMessage(senderId,receiverId)
+    ): Flow<ChatRoomData> = chatDataSource.getChatMessage(senderId,receiverId)
 
 }
