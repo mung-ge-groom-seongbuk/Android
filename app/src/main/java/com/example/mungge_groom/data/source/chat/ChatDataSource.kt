534f330fe7bd2029f6531b92e4f15617c17b1b54
jiwon <pjw99032986@gmail.com>
2024-10-11 00:31:27 +0900

    feat : 채팅 api 서비스 데이터 소스

--- app/src/main/java/com/example/mungge_groom/data/source/chat/ChatDataSource.kt
+++ app/src/main/java/com/example/mungge_groom/data/source/chat/ChatDataSource.kt
@@ -4,6 +4,8 @@ import android.util.Log
 import com.example.mungge_groom.data.remote.ChatApi
 import com.example.mungge_groom.data.request.NotificationDTO
 import com.example.mungge_groom.data.request.SendChatDTO
+import com.example.mungge_groom.data.response.ChatRoomData
+import com.example.mungge_groom.data.response.ChatRoomResponse
 import com.example.mungge_groom.ui.base.BaseResponse
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
@@ -13,14 +15,14 @@ import javax.inject.Inject
 class ChatDataSource @Inject constructor(
     private val chatApi: ChatApi
 ) {
-    fun postSendChat(sendChatDTO: SendChatDTO): Flow<BaseResponse<String>> = flow {
+    fun postSendChat(sendChatDTO: SendChatDTO): Flow<ChatRoomResponse> = flow {
         val result = chatApi.postSendChat(sendChatDTO)
         emit(result)
     }.catch {
         Log.e("postSendChat Failure", it.message.toString())
     }
 
-    fun getChatMessage(senderId: String, receiverId: String): Flow<BaseResponse<String>> = flow {
+    fun getChatMessage(senderId: String, receiverId: String): Flow<ChatRoomData> = flow {
         val result = chatApi.getChatMessage(senderId, receiverId)
         emit(result)
     }.catch {
