ac3b001d65b702af3cfc3e1ce7d0595a42cf22b8
jiwon <pjw99032986@gmail.com>
2024-10-11 13:07:07 +0900

    feat : 알림 연동

--- app/src/main/java/com/example/mungge_groom/data/model/NotificationData.kt
+++ app/src/main/java/com/example/mungge_groom/data/model/NotificationData.kt
@@ -6,5 +6,5 @@ data class NotificationData(
     val title : String = "수박에게 매칭요청이 왔어요!",
     val description : String = "오늘 저녁에 같이 러닝 어떠세요?",
     val arriveTime : String = "4",
-    val isPermission : Boolean = true
+    var isPermission : Boolean = true
 )
