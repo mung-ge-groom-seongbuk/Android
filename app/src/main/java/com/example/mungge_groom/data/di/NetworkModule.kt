fc3878186b5f3197a23a911e15450463d92b95e9
jiwon <pjw99032986@gmail.com>
2024-10-11 13:08:51 +0900

    feat : 토큰, 쿠키 분리

--- app/src/main/java/com/example/mungge_groom/data/di/NetworkModule.kt
+++ app/src/main/java/com/example/mungge_groom/data/di/NetworkModule.kt
@@ -1,13 +1,18 @@
 package com.example.mungge_groom.data.di
 
+import com.example.mungge_groom.extention.TokenManager
+import com.example.mungge_groom.utils.AccessTokenInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.JavaNetCookieJar
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.net.CookieManager
+import java.net.CookiePolicy
 import java.util.concurrent.TimeUnit
 import javax.inject.Named
 import javax.inject.Qualifier
@@ -16,11 +21,15 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+    @Singleton
+    @Provides
+    fun provideAccessTokenInterceptor(tokenManager: TokenManager): AccessTokenInterceptor {
+        return AccessTokenInterceptor(tokenManager)
+    }
 
     @Qualifier
     @Retention(AnnotationRetention.BINARY)
     annotation class MainRetrofit
-
 
     @Provides
     @Singleton
@@ -36,17 +45,28 @@ object NetworkModule {
 
     @Provides
     @Singleton
+    fun provideCookieManager(): CookieManager {
+        return CookieManager().apply {
+            setCookiePolicy(CookiePolicy.ACCEPT_ALL)
+        }
+    }
+
+    @Provides
+    @Singleton
     fun provideOkHttpClient(
         httpLoggingInterceptor: HttpLoggingInterceptor,
+        accessTokenInterceptor: AccessTokenInterceptor,
+        cookieManager: CookieManager
     ): OkHttpClient {
         return OkHttpClient.Builder()
             .connectTimeout(30, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
             .writeTimeout(30, TimeUnit.SECONDS)
             .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(accessTokenInterceptor)
+            .cookieJar(JavaNetCookieJar(cookieManager))
             .build()
     }
-
 
     @MainRetrofit
     @Singleton
