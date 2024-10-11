7cb8400bcf71334cbfd9617b0458295024a9486d
jiwon <pjw99032986@gmail.com>
2024-10-11 13:09:30 +0900

    feat : 러닝 데이터

---
+++ app/src/main/java/com/example/mungge_groom/data/response/GetRecordRunData.kt
@@ -0,0 +1,12 @@
+package com.example.mungge_groom.data.response
+
+data class GetRecordRunData(
+    val calories: String="",
+    val distance_km: String="",
+    val duration: String="",
+    val pace: String="",
+    val recorded_at: String="",
+    val run_count: String="",
+    val run_id: String="",
+    val user_id: String=""
+)
