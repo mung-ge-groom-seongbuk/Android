078cee026cd346932b42546513dba71cc53dc3e5
jiwon <pjw99032986@gmail.com>
2024-10-11 13:07:28 +0900

    feat : jwt레포지토리 추가 (세션, 토큰 분리)

--- app/src/main/java/com/example/mungge_groom/data/di/RepositoryModule.kt
+++ app/src/main/java/com/example/mungge_groom/data/di/RepositoryModule.kt
@@ -3,12 +3,18 @@ package com.example.mungge_groom.data.di
 import com.example.mungge_groom.data.repository.AccountRepository
 import com.example.mungge_groom.data.repository.ChatRepository
 import com.example.mungge_groom.data.repository.HomeRepository
+import com.example.mungge_groom.data.repository.JwpRepository
+import com.example.mungge_groom.data.repository.MypageRepository
 import com.example.mungge_groom.data.source.account.AccountApiDataSource
 import com.example.mungge_groom.data.source.account.AccountApiRepositoryImpl
 import com.example.mungge_groom.data.source.chat.ChatDataSource
 import com.example.mungge_groom.data.source.chat.ChatRepositoryImpl
 import com.example.mungge_groom.data.source.home.HomeDataSource
 import com.example.mungge_groom.data.source.home.HomeRepositoryImpl
+import com.example.mungge_groom.data.source.jwt.JwtDataSource
+import com.example.mungge_groom.data.source.jwt.JwtRepositoroyImpl
+import com.example.mungge_groom.data.source.mypage.MypageDataSource
+import com.example.mungge_groom.data.source.mypage.MypageRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -28,10 +34,20 @@ object RepositoryModule {
     @Provides
     fun provideHomeApiRepository(homeDataSource: HomeDataSource): HomeRepository =
         HomeRepositoryImpl(homeDataSource)
-
     @Singleton
     @Provides
     fun provideChatApiRepository(chatDataSource: ChatDataSource): ChatRepository =
         ChatRepositoryImpl(chatDataSource)
+
+    @Singleton
+    @Provides
+    fun provideMyPageRepository(mypageDataSource: MypageDataSource): MypageRepository =
+        MypageRepositoryImpl(mypageDataSource)
+
+    @Singleton
+    @Provides
+    fun provideJwtRepository(jwtDataSource: JwtDataSource): JwpRepository =
+        JwtRepositoroyImpl(jwtDataSource)
+
 
 }
