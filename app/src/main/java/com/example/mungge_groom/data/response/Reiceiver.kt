3721b02c4ab783457c8119f06bbc4082d80daefa
jiwon <pjw99032986@gmail.com>
2024-10-11 13:07:47 +0900

    feat : 소켓통신 , 채팅 연동

---
+++ app/src/main/java/com/example/mungge_groom/data/response/Reiceiver.kt
@@ -0,0 +1,7 @@
+package com.example.mungge_groom.data.response
+
+data class Reiceiver(
+    val nickname: String="",
+    val profile_picture: String="",
+    val user_id: String=""
+)
---
+++ app/src/main/java/com/example/mungge_groom/ui/chat/Receiver.kt
@@ -0,0 +1,7 @@
+package com.example.mungge_groom.ui.chat
+
+data class Receiver(
+    val nickname: String,
+    val profile_picture: String,
+    val user_id: Int
+)
